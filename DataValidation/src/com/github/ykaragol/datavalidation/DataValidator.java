95742cd92eaed5bee7d82043d7866051c8ee99ba
ykaragol <>
2015-02-08 23:02:20 +0200

    Names changed

--- DataValidation/src/com/github/ykaragol/datavalidation/DataValidator.java
+++ DataValidation/src/com/github/ykaragol/datavalidation/DataValidator.java
@@ -1,93 +1,21 @@
 package com.github.ykaragol.datavalidation;
 
 
-import static org.junit.Assert.fail;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.GeneralSecurityException;
 import java.util.List;
 
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.poifs.crypt.Decryptor;
-import org.apache.poi.poifs.crypt.EncryptionInfo;
-import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataValidationConstraint;
 import org.apache.poi.ss.usermodel.DataValidationConstraint.ValidationType;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.util.CellRangeAddress;
 import org.apache.poi.ss.util.CellRangeAddressList;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFDataValidation;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.junit.Assert;
-import org.junit.Test;
 
-public class DataValidation {
-
-	public static final String PATH = "./test-files/";
-
-	@Test
-	public void testDataValidation() {
-		String fileName = "DataValidationTest.xlsx";
-
-		InputStream is = null;
-		boolean isValid = true;
-		try {
-			is = new FileInputStream(new File(PATH + fileName));
-
-			XSSFWorkbook wb = openWorkbook(convertEncryptedXlsx2DecryptedXlsx(is));
-			XSSFSheet sheet = wb.getSheetAt(0);
-
-			visitSheet(sheet);
-
-		} catch (FileNotFoundException e) {
-			fail("FileNotFound");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
-			closeSilently(is);
-		}
-		Assert.assertEquals("isValid ", true, isValid);
-	}
-
-	public static XSSFWorkbook openWorkbook(InputStream is) {
-		Workbook wb;
-		try {
-			wb = WorkbookFactory.create(is);
-			if (wb instanceof XSSFWorkbook) {
-				return (XSSFWorkbook) wb;
-			}
-			throw new RuntimeException();
-		} catch (InvalidFormatException e) {
-			throw new RuntimeException();
-		} catch (IOException e) {
-			throw new RuntimeException();
-		}
-	}
-
-	public static InputStream convertEncryptedXlsx2DecryptedXlsx(InputStream is) throws IOException {
-		POIFSFileSystem fs = new POIFSFileSystem(is);
-		EncryptionInfo info = new EncryptionInfo(fs);
-		Decryptor d = Decryptor.getInstance(info);
-		try {
-			d.verifyPassword(Decryptor.DEFAULT_PASSWORD);
-			InputStream dataStream = d.getDataStream(fs);
-			return dataStream;
-		} catch (GeneralSecurityException e) {
-			throw new IOException(e);
-		}
-	}
+public class DataValidator {
 	
-	private void visitSheet(XSSFSheet sheet) {
+	public void visitSheet(XSSFSheet sheet) {
 		List<XSSFDataValidation> dataValidations = sheet.getDataValidations();
 		for (XSSFDataValidation xssfDataValidation : dataValidations) {
 			DataValidationConstraint validationConstraint = xssfDataValidation.getValidationConstraint();
@@ -129,15 +57,7 @@ public class DataValidation {
 		return a < b ? a : b;
 	}
 
-	private void closeSilently(InputStream is) {
-		if (is != null) {
-			try {
-				is.close();
-			} catch (IOException e) {
-				/* silent */
-			}
-		}
-	}
+
 
 	private void checkCell(DataValidationConstraint validationConstraint, XSSFCell cell) {
 		System.err.println(cell.getReference() + ": "  + cell.getRawValue());
